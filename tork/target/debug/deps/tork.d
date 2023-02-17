078ae7f0d05dd502dd06b1b6c6a82983d2bb379d
Tyson Crisp <fnv359@outlook.com>
2023-02-17 16:54:07 -0600

    Creation of the Cargo crate;
    So far, just a simple "Hello World" program

---
+++ tork/target/debug/deps/tork.d
@@ -0,0 +1,5 @@
+C:\Repos\rusty-tork\tork\target\debug\deps\tork.exe: src\main.rs
+
+C:\Repos\rusty-tork\tork\target\debug\deps\tork.d: src\main.rs
+
+src\main.rs:
---
+++ tork/target/debug/tork.d
@@ -0,0 +1,1 @@
+C:\Repos\rusty-tork\tork\target\debug\tork.exe: C:\Repos\rusty-tork\tork\src\main.rs
