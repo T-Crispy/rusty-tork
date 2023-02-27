66681087b7e332bb431f28ebe7315155fedd43bf
Tyson Crisp <fnv359@gmail.com>
2023-02-26 22:16:23 -0600

    v0.1.0.1 - set up module tree for the basic objects

--- tork/src/main.rs
+++ tork/src/main.rs
@@ -1,3 +1,13 @@
+pub mod builder;
+pub mod driver;
+pub mod world;
+
+/*
+___make builder capable to build source file
+    >rooms will only have NESW directions
+___make driver capable of playing through a world
+*/
+
 fn main() {
     println!("Hello, world!");
 }
