8552bd30cb8956dc1faa9b85d17faea98094f687
Tyson Crisp <q867g498@wichita.edu>
2023-03-13 23:27:17 -0500

    v0.1.2.1 - just removing a comment block that I forgot to remove; no need to change the version number for this

--- tork/src/builder.rs
+++ tork/src/builder.rs
@@ -120,51 +120,5 @@ pub fn build_world(filename: String) -> (World, String) {
         return (to_build, String::from("err: number of lines between #rooms and #paths headers are off. Double check the room lines"));
     }
 
-    /*
-    //~~~~~~~~~~~~~~~~~~prev code~~~~~~~~~~~~~~~~~~~~~
-    if lines.len() > min_source_lines {
-        //get name of world
-        to_build.name = lines[0].trim_matches('\r').to_string();
-        //get # of rooms
-        let num_rooms: usize = lines[1].trim().parse::<usize>().unwrap();
-        
-        let mut curr_line: usize = 2;
-
-        //get id, name, & desc of each room
-        for room_iter in 0..num_rooms {
-            let i = room_iter as usize;
-            let mut curr_room: Room = Room {
-                name: String::from(""),
-                desc: String::from(""),
-                id: 0,
-                pathways: [ptr::null_mut(), 
-                            ptr::null_mut(), 
-                            ptr::null_mut(),
-                            ptr::null_mut()]
-            };
-
-            curr_room.id = lines[curr_line + (i * 3)].trim().parse::<usize>().unwrap();
-            curr_room.name = lines[curr_line + (i * 3) + 1].trim_matches('\r').to_string();
-            curr_room.desc = lines[curr_line + (i * 3) + 2].trim_matches('\r').to_string();
-            to_build.rooms.push(curr_room);
-        }
-
-        curr_line = curr_line + (num_rooms * 3);
-
-        //get pathways for each room
-        for room_iter in 0..num_rooms {
-            let i = room_iter as usize;
-            
-            let paths: Vec<&str> = lines[curr_line + i].trim().split('|').collect();
-            for j in 0..4 {
-                if paths[j] != "NULL" {
-                    let num_id: usize = paths[j].trim().parse::<usize>().unwrap();
-                    let room_id: usize = to_build.get_room_index(num_id).0;
-                    to_build.rooms[i].pathways[j] = &mut to_build.rooms[room_id];
-                }
-            }
-        }
-    }*/
-
     (to_build,String::from("success"))
 }
